526bd09f41fb4f659eab16d71250873fc0299308
RavikantMehta <140971803+RavikantMehta@users.noreply.github.com>
2025-04-18 16:28:18 +0530

    Update5 main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -1,69 +1,72 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <string>
+#include <sstream>
 #include <boost/asio.hpp>
+
 #include <openpal/logging/LogLevels.h>
-#include <opendnp3/outstation/IOutstation.h>
-#include <opendnp3/outstation/OutstationStackConfig.h>
-#include <opendnp3/outstation/OutstationConfig.h>
-#include <opendnp3/outstation/IOutstationApplication.h>
-#include <opendnp3/outstation/DatabaseConfig.h>
 #include <opendnp3/LogLevels.h>
-#include <opendnp3/AnalogOutput.h>
+#include <opendnp3/IPEndpoint.h>
+#include <opendnp3/DatabaseConfig.h>
+#include <opendnp3/MeasurementTypes.h>
+#include <opendnp3/EventType.h>
 #include <opendnp3/Flags.h>
 #include <opendnp3/EventMode.h>
-#include <asiodnp3/DefaultOutstationApplication.h>
+
 #include <asiodnp3/DNP3Manager.h>
+#include <asiodnp3/DefaultOutstationApplication.h>
 #include <asiodnp3/OutstationStackConfig.h>
+#include <asiodnp3/IOutstation.h>
 #include <asiodnp3/UpdateHandlers.h>
 
 using boost::asio::ip::tcp;
 
-const int SENSOR_PORT = 20001;
-const int SCADA_PORT = 20000;
+const int TCP_PORT = 20001;
 
-std::mutex update_mutex;
+std::mutex db_mutex;
+double latest_temp = 0.0;
+double latest_pressure = 0.0;
+double latest_humidity = 0.0;
 
-void UpdateAnalog(asiodnp3::IOutstation* outstation, double value, uint16_t index)
-{
-    std::lock_guard<std::mutex> lock(update_mutex);
-    opendnp3::Analog analog(value, opendnp3::Flags(0x01)); // ONLINE
-    outstation->Update(analog, index, opendnp3::EventMode::Detect);
-    std::cout << "[OUTSTATION] Analog[" << index << "] = " << value << std::endl;
-}
-
-void StartSensorTCP(asiodnp3::IOutstation* outstation)
+// Thread function to receive TCP sensor data
+void TCPReceiver()
 {
     boost::asio::io_context io;
-    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), SENSOR_PORT));
-    std::cout << "[TCP] Listening for sensor data on port " << SENSOR_PORT << "...\n";
+    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), TCP_PORT));
+
+    std::cout << "[TCP] Listening on port " << TCP_PORT << "...\n";
 
     while (true)
     {
         tcp::socket socket(io);
         acceptor.accept(socket);
 
-        boost::asio::streambuf buffer;
-        boost::asio::read_until(socket, buffer, '\n');
+        boost::asio::streambuf buf;
+        boost::asio::read_until(socket, buf, '\n');
 
-        std::istream input(&buffer);
+        std::istream input(&buf);
         std::string line;
         std::getline(input, line);
 
-        std::stringstream ss(line);
         double temp, pressure, humidity;
-        char comma;
+        char comma1, comma2;
 
-        if (ss >> temp >> comma >> pressure >> comma >> humidity)
+        std::stringstream ss(line);
+        if (ss >> temp >> comma1 >> pressure >> comma2 >> humidity)
         {
-            std::cout << "[RECEIVED] " << temp << ", " << pressure << ", " << humidity << std::endl;
-            UpdateAnalog(outstation, temp, 0);
-            UpdateAnalog(outstation, pressure, 1);
-            UpdateAnalog(outstation, humidity, 2);
+            std::lock_guard<std::mutex> lock(db_mutex);
+            latest_temp = temp;
+            latest_pressure = pressure;
+            latest_humidity = humidity;
+
+            std::cout << "[RECEIVED] Temp: " << temp
+                      << ", Pressure: " << pressure
+                      << ", Humidity: " << humidity << "\n";
         }
         else
         {
-            std::cerr << "[ERROR] Could not parse: " << line << std::endl;
+            std::cerr << "[ERROR] Invalid data: " << line << "\n";
         }
     }
 }
@@ -71,43 +74,52 @@ void StartSensorTCP(asiodnp3::IOutstation* outstation)
 int main()
 {
     const auto logLevels = openpal::LogLevels::NORMAL | openpal::LogLevels::ALL_APP_COMMS;
-
     asiodnp3::DNP3Manager manager(1);
-    auto log = manager.GetLogger();
-    log.SetLevels(logLevels);
-
-    auto channel = manager.AddTCPServer(
-        "dnp3_server",
-        logLevels,
+    auto channel = manager.AddTCPServer("server", logLevels,
         opendnp3::ServerAcceptMode::CloseNew,
-        { opendnp3::IPEndpoint("0.0.0.0", SCADA_PORT) },
+        { opendnp3::IPEndpoint("0.0.0.0", 20000) }, // SCADA listens here
         std::chrono::seconds(5)
     );
 
-    opendnp3::DatabaseConfig dbConfig(10);
-    dbConfig.analog[0].clazz = opendnp3::PointClass::Class1;
-    dbConfig.analog[1].clazz = opendnp3::PointClass::Class1;
-    dbConfig.analog[2].clazz = opendnp3::PointClass::Class1;
+    opendnp3::DatabaseConfig db(10);
+    db.analog[0].clazz = opendnp3::PointClass::Class1;
+    db.analog[1].clazz = opendnp3::PointClass::Class1;
+    db.analog[2].clazz = opendnp3::PointClass::Class1;
 
-    asiodnp3::OutstationStackConfig stackConfig(dbConfig);
-    stackConfig.outstation.params.allowUnsolicited = false;
-    stackConfig.outstation.params.eventBufferConfig = opendnp3::EventBufferConfig(10);
+    asiodnp3::OutstationStackConfig config(db);
+    config.outstation.params.allowUnsolicited = false;  // Only respond to poll
+    config.outstation.params.eventBufferConfig = opendnp3::EventBufferConfig(10);
 
-    auto outstation = channel->AddOutstation(
-        "outstation",
+    auto outstation = channel->AddOutstation("outstation",
         asiodnp3::UpdateHandlers::Create(),
         asiodnp3::DefaultOutstationApplication::Create(),
-        stackConfig
-    );
+        config);
 
     outstation->Enable();
-    std::cout << "[INFO] DNP3 Outstation running on port " << SCADA_PORT << "\n";
+    std::cout << "[DNP3] Outstation enabled\n";
 
-    std::thread sensorThread(StartSensorTCP, outstation.get());
-    sensorThread.join();
+    // Start TCP thread
+    std::thread tcpThread(TCPReceiver);
 
+    // Poll-safe loop to push data as Class 1 events
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        {
+            std::lock_guard<std::mutex> lock(db_mutex);
+            asiodnp3::UpdateBuilder builder;
+            builder.Update(opendnp3::Analog(latest_temp, opendnp3::Flags(0x01)), 0);
+            builder.Update(opendnp3::Analog(latest_pressure, opendnp3::Flags(0x01)), 1);
+            builder.Update(opendnp3::Analog(latest_humidity, opendnp3::Flags(0x01)), 2);
+            outstation->Apply(builder.Build());
+        }
+    }
+
+    tcpThread.join();
     return 0;
 }
+
 
 
 
