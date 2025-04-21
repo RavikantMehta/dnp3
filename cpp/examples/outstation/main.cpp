7d0e4a027ec9f0303281aff70aad9426d96d6ce5
RavikantMehta <140971803+RavikantMehta@users.noreply.github.com>
2025-04-21 11:00:46 +0530

    Update final commit 2 main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -76,7 +76,7 @@ void ReceiveSensorData(std::shared_ptr<IOutstation> outstation)
 {
     try {
         boost::asio::io_context io_context;
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 15000));
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 20001));
         std::cout << "[INFO] Listening for sensor data on port 20001..." << std::endl;
 
         while (true)
